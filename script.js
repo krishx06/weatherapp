7cdd9821e46273bdb027c3d7bcb488045d5f60ea
krishx06 <krishpatil120@gmail.com>
2025-03-27 01:02:45 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,31 @@
+const apiKey = "6aa4ad9420b3f3a4ee38104f6e2deb2a";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    if (response.status === 404) {
+        alert("City not found. Please enter a valid city name.");
+        return;
+    }
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"; 
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+VanillaTilt.init(document.querySelector(".card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.3,
+});
